5a9b90d39bf9a8e3211396054604e43afbd3cea8
duidelijkedatal <duidelijkedata@gmail.com>
2026-02-05 14:16:16 +0100

    fix

--- app/ouder/share/[code]/page.tsx
+++ app/ouder/share/[code]/page.tsx
@@ -1,6 +1,18 @@
-import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import ShareClient from "./share-client";
 
-export default function ShareIndex() {
-  redirect("/ouder"); // of "/ouder/koppelen" wat jij logisch vindt
+export default async function Page(props: { params: Promise<{ code: string }> }) {
+  const { code } = await props.params;
+
+  return (
+    <Suspense
+      fallback={
+        <div className="h-screen w-screen flex items-center justify-center bg-black text-white">
+          Laden…
+        </div>
+      }
+    >
+      <ShareClient code={code} />
+    </Suspense>
+  );
 }
-
